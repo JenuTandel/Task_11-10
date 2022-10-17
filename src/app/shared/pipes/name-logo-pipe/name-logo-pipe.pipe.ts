3aacec96d6f6d1d5403b362860e02177e3a88c1d
Jinal Tandel <jinaltandel06@gmail.com>
2022-10-17 14:14:00 +0530

    fix:logo name without space

--- src/app/shared/pipes/name-logo-pipe/name-logo-pipe.pipe.ts
+++ src/app/shared/pipes/name-logo-pipe/name-logo-pipe.pipe.ts
@@ -5,8 +5,15 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class NameLogoPipePipe implements PipeTransform {
   transform(value: string): string {
+    let logo="";
     value = value.toUpperCase();
-    const nameArray = value.split(' ');
-    return `${nameArray[0].charAt(0)}${nameArray[1].charAt(0)}`;
+    if(value.indexOf(' ')>=0){
+      const nameArray = value.split(' ');
+      logo = `${nameArray[0].charAt(0)}${nameArray[1].charAt(0)}`;
+    }
+    else{
+      logo = value.substring(0,2);
+    }
+    return logo;
   }
 }
